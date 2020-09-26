ffce64be9b87314350cdfccd51e8608860da987c
HadiNiazi <hadiniazi801@gmail.com>
2020-09-26 18:20:59 +0500

    Laravel POS

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+    }
+}
